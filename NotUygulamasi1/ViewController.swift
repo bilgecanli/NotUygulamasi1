7869bbbddcf679a1e6b46ebddadce875c84c5db3
mehmet <mehmetsaidcanli@gmail.com>
2023-12-11 15:35:03 +0300

    Initial Commit

---
+++ NotUygulamasi1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotUygulamasi1
+//
+//  Created by Mehmet Said Canlı on 11.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
